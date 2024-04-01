956e590bff7a999f3a87451bc9900c36ceb81010
singhritesh85 <56765895+singhritesh85@users.noreply.github.com>
2024-04-01 21:43:19 +0530

    Add files via upload

---
+++ azure-aks-withoutmanagedprometheusgrafana/module/variables.tf
@@ -0,0 +1,24 @@
+variable "prefix" {
+
+}
+
+variable "location" {
+
+}
+
+variable "kubernetes_version" {
+
+}
+
+variable "ssh_public_key" {
+
+}
+
+variable "action_group_shortname" {
+
+}
+
+variable "env" {
+
+}
+
